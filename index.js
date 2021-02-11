ca017367c43079b8cadffd399ccf288eefc4f803
Anirach Mingkhwan <Anirach.m@fitm.kmutnb.ac.th>
2021-02-11 10:05:54 +0700

    Start

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Hello Welcome ....to Node.js World!");
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
